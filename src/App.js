2090d60882c49f23e09a26e33b9cd3a97fb6926f
Guy Archibald <guyarchibald@Guys-MacBook-Pro.local>
2021-05-22 00:04:48 -0500

    Removes p tag

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello World, I'm Miles!</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
