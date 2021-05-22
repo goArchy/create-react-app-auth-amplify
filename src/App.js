366300a50f90c8300aa183e511a60fc4490d7eea
Guy Archibald <guyarchibald@Guys-MacBook-Pro.local>
2021-05-21 23:38:32 -0500

    Adds backend api, updates project config

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello World, I'm Miles!</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
